b7e86db427f1412b7764245dfe920746cd73e952
ramirezbryanchezter <ramirezbryanchezter@gmail.com>
2024-07-17 00:26:06 +0800

    Initial layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import DisplayMedia from './components/DisplayMedia';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  const [data, setData] = React.useState({
+    copyright: null,
+    date: null,
+    explanation: null,
+    hdurl: null,
+    media_type: null,
+    service_version: null,
+    title: null,
+    url: null,
+  });
+  
+  React.useEffect(() => {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+    .then(res => res.json()).then(
+      data => {
+        setData({
+          copyright: data.copyright,
+          date: data.date,
+          explanation: data.explanation,
+          hdurl: data.hdurl,
+          media_type: data.media_type,
+          service_version: data.service_version,
+          title: data.title,
+          url: data.url
+        })
+      }
+    )
+  }, [])
+
+  console.log(data);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Astronomy Pictures by NASA</h1>
+    <p>View the daily photos from NASA's APOD API.</p>
+    <p>You can also choose to view a photo from a previous date, or several photos from a date range.</p>
+    <DisplayMedia {...data}/>
     </>
   )
 }
